004aa0c1eb8229c589c3edb60c16531c8e0ed449
rafael ortiz <35827025+dirakx1@users.noreply.github.com>
2019-11-20 13:18:35 -0500

    Update setgkeenv.sh

--- setgkeenv.sh
+++ setgkeenv.sh
@@ -1,4 +1,5 @@
 # Script to ser a simple GKE env to interact with k8s
+# probably you should issue gcloud init first
 
 gcloud config set project "nameofproject"
 gcloud container clusters get-credentials "nameofcluster" --zone=zoneofcluster
